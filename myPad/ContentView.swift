669aa465c9645a26f2ececf0c7eb6d06d068f1d5
Josh Goldwasser <josh@ciderhouse.org>
2026-05-17 10:31:30 -0700

    Initial Commit

---
+++ myPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myPad
+//
+//  Created by Kassidy Voss on 5/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myPad/myPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPadApp.swift
+//  myPad
+//
+//  Created by Kassidy Voss on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct myPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
